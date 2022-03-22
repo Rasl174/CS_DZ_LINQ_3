caf00fb4503aeb2dfed5905420049c155c03043c
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-22 13:49:04 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_LINQ_3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_LINQ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+
+            Console.WriteLine("Это регистратура.");
+
+            List<Patient> patients = new List<Patient> { new Patient("Иванов Иван", 58, "ОРЗ"), new Patient("Петрович Петр", 77, "Грипп"),
+            new Patient("Алексеевский Алексей", 21, "ОРЗ"), new Patient("Пирожков Артур", 50 , "Грипп"), new Patient("Дабудаев Дабуди", 11, "Грипп"),
+            new Patient("Экстровертов Интроверт", 15, "Грипп"), new Patient("Балансова Кардан", 27, "ОРЗ"), new Patient("Потеков Шрус", 43, "ОРЗ"),
+            new Patient("Салават Юлаев", 28, "Перелом"), new Patient("Кривожопов Олень", 25, "Перелом")};
+
+            while (isWork)
+            {
+                Console.WriteLine("Для сортировки больных по ФИО введите 1");
+                Console.WriteLine("Для сортировки больных по возрасту введите 2");
+                Console.WriteLine("Для сортировки больных по заболеванию введите 3");
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        var sorteredNamePatients = patients.OrderBy(patient => patient.Name);
+                        foreach (var patient in sorteredNamePatients)
+                        {
+                            Console.WriteLine(patient.Name + " " + patient.Age + " " + patient.Disease);
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        var sorteredAgePatients = patients.OrderBy(patient => patient.Age);
+                        foreach (var patient in sorteredAgePatients)
+                        {
+                            Console.WriteLine(patient.Name + " " + patient.Age + " " + patient.Disease);
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("Введите заболевание: ");
+                        string userInputDisease = Console.ReadLine();
+                        var filteredDiseasePatients = patients.Where(patient => patient.Disease == userInputDisease).OrderBy(patient => patient.Disease);
+                        foreach (var patient in filteredDiseasePatients)
+                        {
+                            Console.WriteLine(patient.Name + " " + patient.Age + " " + patient.Disease);
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+
+    class Patient
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Disease { get; private set; }
+
+        public Patient(string name, int age, string disease)
+        {
+            Name = name;
+            Age = age;
+            Disease = disease;
+        }
+    }
+}
